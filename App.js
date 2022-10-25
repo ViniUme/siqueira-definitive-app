b109d5b5426350cb39d661a3189138ab2ccb3f2d
Vinícius Henrique dos Santos <66230638+ViniUme@users.noreply.github.com>
2022-10-25 17:16:52 -0300

    Fixing again the error that font import

--- App.js
+++ App.js
@@ -1,40 +1,23 @@
-import * as Font from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
 import { Text, View } from 'react-native';
-import { useEffect, useCallback } from 'react';
+import { useFonts, Inter_900Black, Inter_500Medium } from '@expo-google-fonts/inter';
 
 export default function App() {
-  const [fontsLoaded] = Font.useFonts({
-    'Inter-Medium': require('./assets/fonts/Inter/Inter-Medium.ttf'),
-    'Inter-Bold': require('./assets/fonts/Inter/Inter-Bold.ttf'),
-    'Roboto-Medium': require('./assets/fonts/Roboto/Roboto-Medium.ttf'),
-    'Roboto-Bold': require('./assets/fonts/Roboto/Roboto-Bold.ttf')
+  let [fontsLoaded] = useFonts({
+    Inter_900Black,
+    Inter_500Medium
   });
 
-  useEffect(() => {
-    async function prepare() {
-      await SplashScreen.preventAutoHideAsync();
-    }
-
-    prepare();
-  }, []);
-
-  const onLayoutRootView = useCallback(async () => {
-    if (fontsLoaded) {
-      await SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded]);
-
   if (!fontsLoaded) {
-    return null;
+    return
   }
-
   return (
-    <View 
-      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-      onLayout={onLayoutRootView}>
-      <Text>Platform Default</Text>
-      <Text style={{ fontFamily: 'Inter-Medium' }}>Inter Black</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ fontFamily: 'Inter_900Black' }}>
+        Inter Black
+      </Text>
+      <Text style={{ fontFamily: 'Inter_500Medium' }}>
+        Inter Medium
+      </Text>
     </View>
   );
-}
+};
