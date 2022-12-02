53065baf27045dea154a8dd3e8a94ed85cb60182
Vinícius Henrique dos Santos <66230638+ViniUme@users.noreply.github.com>
2022-12-02 14:28:13 -0300

    Save all alterations

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 import { Text, View } from 'react-native';
-import { useFonts, Inter_900Black, Inter_500Medium } from '@expo-google-fonts/inter';
+import { useFonts, Inter_500Medium } from '@expo-google-fonts/inter';
 
 export default function App() {
   let [fontsLoaded] = useFonts({
-    Inter_900Black,
-    Inter_500Medium
+    Inter_500Medium,
   });
 
   if (!fontsLoaded) {
@@ -12,9 +11,6 @@ export default function App() {
   }
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text style={{ fontFamily: 'Inter_900Black' }}>
-        Inter Black
-      </Text>
       <Text style={{ fontFamily: 'Inter_500Medium' }}>
         Inter Medium
       </Text>
